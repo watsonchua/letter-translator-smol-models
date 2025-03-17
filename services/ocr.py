74a947caa9baeea5152cfe1fe74d1700fdead05d
Watson Chua <watsonchua@gmail.com>
2025-03-17 19:19:06 +0800

    convert responses to dict

--- services/ocr.py
+++ services/ocr.py
@@ -1,13 +1,18 @@
 from services.client import jigsaw_client
+import json
 from jigsawstack import JigsawStackError
 
 def extract_content(url_to_filepath, prompt_list):
     try:
         ocr_response = jigsaw_client.vision.vocr({"url": url_to_filepath, "prompts" : prompt_list})
-        response_text = ocr_response["context"]
+        # print(ocr_response.json())
+        ocr_response_dict = ocr_response.json() 
+        response_text = ocr_response_dict["context"]
     except KeyError as e:
         print(e)
-        return None
+        # return everything except the sections ,width, height
+        response_text = json.loads({k:v for k,v in ocr_response_dict.items() if k not in ["sections", "width", "height", "success", "tags"]})
+
     except JigsawStackError as e:
         print(f"An error occurred during uploading: {e}")
         return None      
--- services/translate.py
+++ services/translate.py
@@ -3,9 +3,11 @@ from services.client import jigsaw_client
 # zh for chinese, en for english
 def translate_text(text, target_language, current_language):
     try:
-        translated_text = jigsaw_client.translate({"text": text, "target_language": target_language, "current_language": current_language })
+        translated_text_response = jigsaw_client.translate({"text": text, "target_language": target_language, "current_language": current_language })
+        translated_text_dict = translated_text_response.json()
+
     except Exception as e:
         print(f"Translation failed because of {str(e)} Returning original text.")
         return text
 
-    return translated_text["translated_text"]
+    return translated_text_dict["translated_text"]
